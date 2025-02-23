c9b5a0e0240e1773b9068112d494abeddb9316a8
somrajroy <somraj.sinha.roy@gmail.com>
2025-02-23 19:00:13 +0530

    github actions

--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#     }
-#   }
-# }
+provider "aws" {
+  region = "us-west-2"
+}
 
-# provider "aws" {
-#   region     = "us-west-2"
-#   access_key = "PUT_YOUR_ACCESS_KEY"
-#   secret_key = "PUT_YOUR_SECRET_KEY"
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7"
+    }
+  }
+}
+

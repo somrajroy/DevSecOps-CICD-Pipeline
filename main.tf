82f682f302db7d20fdf2e75e0f9b74516f3f8f33
somrajroy <somraj.sinha.roy@gmail.com>
2025-02-23 19:09:55 +0530

    updates

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 #tfsec:ignore:aws-s3-enable-bucket-logging
-resource "aws_s3_bucket" "example" {
-  bucket = "tfstategithubactions"
+resource "aws_s3_bucket" "tfseclint" {
+  bucket = "tfstategithubactions123"
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_ownership_controls" "tfseclint" {
+  bucket = aws_s3_bucket.tfseclint.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
+resource "aws_s3_bucket_acl" "tfseclint" {
+  depends_on = [aws_s3_bucket_ownership_controls.tfseclint]
 
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.tfseclint.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "tfseclint" {
+  bucket = aws_s3_bucket.tfseclint.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -26,25 +26,25 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_versioning" "versioning_tfseclint" {
+  bucket = aws_s3_bucket.tfseclint.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_kms_key" "mykey" {
+resource "aws_kms_key" "tfseclintkey" {
   description             = "This key is used to encrypt bucket objects"
   enable_key_rotation     = true
   deletion_window_in_days = 7
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfseclint" {
+  bucket = aws_s3_bucket.tfseclint.id
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
+      kms_master_key_id = aws_kms_key.tfseclintkey.arn
       sse_algorithm     = "aws:kms"
     }
   }
